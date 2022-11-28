6c470c55a11e38c72ae7738c8b50292fc1c8fb23
nurlaily3585 <nurlaily.vendyansyah@gmail.com>
2022-11-29 05:34:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/demoservice/MusicServcice.java
@@ -0,0 +1,33 @@
+package com.example.demoservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.provider.Settings;
+
+import androidx.annotation.Nullable;
+
+public class MusicServcice extends Service {
+    MediaPlayer mp;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+       mp=MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+       mp.setLooping(true);
+       mp.start();
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        mp.stop();
+        super.onDestroy();
+    }
+}
